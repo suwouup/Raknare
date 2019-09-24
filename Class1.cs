a2fe2527b946ba55e8a06729aab05a4d9d82cdcc
suwouup <47558836+suwouup@users.noreply.github.com>
2019-09-24 13:05:32 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace räknare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomerare = new Random();
+            int slump_tal = randomerare.Next(100);
+
+            bool Vinna = false;
+            while (Vinna == false)
+            {
+
+                int tal;
+
+                Console.WriteLine("gissa ett tal mellan 1 och 100");
+                tal = Convert.ToInt32(Console.ReadLine());
+
+
+                if (tal == slump_tal)
+                {
+                    Vinna = true;
+                    Console.WriteLine("talet är rätt grattis");
+                }
+
+                else if (tal > slump_tal)
+                {
+                    Console.WriteLine("talet är för högt");
+                }
+                else if (tal < slump_tal)
+                {
+                    Console.WriteLine("talet är för lågt");
+                }
+            }
+        }
+    }
+}
